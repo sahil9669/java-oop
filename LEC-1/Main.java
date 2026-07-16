419485a792196acc5fc4b2834114d204481b8b0b
Sahil Patil <sahilpatil9889@gmail.com>
2026-07-16 11:54:49 +0530

    Added Lec-1 Folder

---
+++ LEC-1/Main.java
@@ -0,0 +1,54 @@
+public class Main{
+    public static void main(String[] args) {
+        Student student=new Student();
+        // Student student happens at compile time
+        // new Student() happens at run time
+        // new keyword dynamically allocates memory and returns a reference to it
+
+        Student one =new Student();
+        Student two=one;
+
+        one.name="Name in Object one";
+        System.out.println(two.name);
+
+        // The output name was similar to object one name
+        // its because two reference variables are referencing to same object  
+        // so change in one makes change in other
+
+        // The output is "Name in Object one" because both reference variables
+        // (one and two) point to the same Student object.
+        // No new object is created when we write:
+        // Student two = one;
+        // Therefore, any changes made through one are also visible through two.
+
+        Student Sahil=new Student();
+        Sahil =new Student(2,"a",89);
+
+        // what we do above is reassigning 
+        // first the Sahil was pointing to object Student() =====> Sahil---->Student()
+        // then we change the refernce, now Sahil is pointing to Student(two) ====> Sahil---->Student(two)
+        // now the object student() has no reference so it will be removed by garbage collector
+
+
+        /* final keyword */
+
+        // final int a; --> throws an error
+        // final variables must be initialsed
+        // we can not change variables which are declared final
+
+        final int a =10;
+        // a=20; --->Throws Error
+        System.out.println(a);
+
+        final Student kunal=new Student();
+        // kunal=new Student(7,"Patya",90);
+
+        // because we have declared final to kunal we can not reassign it 
+        // but we can do
+
+        kunal.rno=89;
+        kunal.name="jajids";
+
+
+    }
+}
---
+++ LEC-1/Student.java
@@ -0,0 +1,40 @@
+public class Student{
+    int rno;
+    String name;
+    float marks;
+
+    // Above are instance variables
+    // The varibles that are declared inside the class and outside the method are called instance variables
+
+    // Below is constructor.
+    // Constructor is basically a specialized method which invokes automatically when object is created.
+    Student(){
+        this.rno=1;
+        this.name="Sahil";
+        this.marks=99.9f;
+    }
+
+    // we can also call constructor from another constructor with this keyword
+    /* 
+    Student(){
+        this(0,"Default name",89);
+        // this will call the constructor Student(int rno,String name,float marks)
+    }
+    */
+
+    Student(int rno,String name,float marks){
+        this.rno=rno;
+        this.name=name;
+        this.marks=marks;
+    }
+
+    Student(Student other){
+        this.rno=other.rno;
+        this.name=other.name;
+        this.marks=other.marks;
+    }
+
+    // the above constructor are of Student class which takes different arguments and no arguments also which is called constructor overloading
+
+
+}
